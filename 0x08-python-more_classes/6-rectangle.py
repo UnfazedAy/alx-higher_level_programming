7527362b4323332eaaa3885694e3800f89f3dbc5
UnfazedAy <ayomidesoniyi@gmail.com>
2022-07-25 14:34:52 -0700

    How many instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,6 +4,9 @@
 
 class Rectangle:
     """creates a class Rectangle"""
+
+    number_of_instances = 0
+    
     def __init__(self, width=0, height=0):
         """
         Initializes an object imediately it is created
@@ -11,6 +14,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -44,7 +48,7 @@ class Rectangle:
         """public instance method that returns the area of the object"""
         rectangle_area = self.height * self.width
         return rectangle_area
-
+    
     def perimeter(self):
         """public instance method that returns the perimeter of the object"""
         if (self.__height == 0) or (self.width == 0):
@@ -60,14 +64,14 @@ class Rectangle:
         """
         if (self.__height == 0) or (self.width == 0):
             return ("")
-
+        
         rect = []
         for i in range(self.__height):
             [rect.append("#") for j in range(self.__width)]
             if i != self.height - 1:
                 rect.append("\n")
         return ("".join(rect))
-
+    
     def __repr__(self):
         """Return the string representation of the Rectangle."""
         rect = "Rectangle(" + str(self.__width)
@@ -76,5 +80,5 @@ class Rectangle:
 
     def __del__(self):
         """Print a message for every deletion of a Rectangle."""
-        type(self).number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
