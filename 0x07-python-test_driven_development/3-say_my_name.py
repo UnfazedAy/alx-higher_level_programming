2fad1623b23641fa6c70aedec36049df6209f018
Ayomide Soniyi <ayomidesoniyi@gmail.com>
2022-10-04 08:31:25 +0100

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-"""Defines a name-printing function."""
+"""say my name: playing around with strings"""
 
 
-def say_my_name(first_name, last_name=""):
-    """Print a name.
-
-    Args:
-        first_name (str): The first name to print.
-        last_name (str): The last name to print.
-    Raises:
-        TypeError: If either of first_name or last_name are not strings.
+def say_my_name(first_name, last_name):
     """
-    if not isinstance(first_name, str):
-        raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+    Args: first_name, last_name
+    result = My name is first_name, last_name
+    e.g My name is Ayomide Soniyi
+    """
+    if type(first_name) is not str:
+        raise TypeError('first_name must be a string')
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+    print(f"My name is {first_name} {last_name}")
