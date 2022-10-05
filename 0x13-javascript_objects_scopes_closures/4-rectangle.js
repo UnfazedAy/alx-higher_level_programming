2a7b2c39f0546055434248d00bbac780abc3cb47
UnfazedAy <ayomidesoniyi@gmail.com>
2022-10-05 16:15:55 -0700

    A class Rectangle with more methods

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  // Print () - prints the rectangle
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let res = '';
+      for (let j = 0; j < this.width; j++) {
+        res += 'X';
+      }
+      console.log(res);
+    }
+  }
+
+  // rotates the width for the height and vice-versa
+  rotate () {
+    this.width = this.height;
+    this.height = this.width;
+  }
+
+  // multiplies the sides by 2
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
