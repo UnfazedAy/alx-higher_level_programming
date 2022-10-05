670e73b3a9ddfe97b169e393120cf20baf97ba06
UnfazedAy <ayomidesoniyi@gmail.com>
2022-10-05 16:58:43 -0700

    done

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 const { argv } = require('process');
 const row = parseInt(argv[2]);
-let result = '';
 if (isNaN(row)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < row; i++) {
+    let res = '';
     for (let j = 0; j < row; j++) {
-      result += 'X';
+      res += 'X';
     }
-    result += '\n';
+    console.log(res);
   }
-  console.log(result);
 }
