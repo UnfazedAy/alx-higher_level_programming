672439ff36ecdb5b25427b5b532032542459f1ee
UnfazedAy <ayomidesoniyi@gmail.com>
2022-10-07 03:12:53 -0700

    Square#1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,22 @@
 #!/usr/bin/node
-class Square extends require('./5-square') {
+const firstSquare = require('./5-square');
+/**
+ * class Square that defines a square and inherit from the first square
+ * in task 5(5-square,js)
+ **/
+class Square extends firstSquare {
+  /**
+   * charPrint() - Prints Square with the specified character
+   * @param {string} c The specified character.
+   **/
   charPrint (c) {
     if (c === undefined) {
       this.print();
     } else {
-      for (let i = 0; i < this.height; i++) {
+      for (let i = 0; i < this.width; i++) {
         let res = '';
-        for (let j = 0; j < this.width; j++) {
-          res += 'C';
+        for (let j = 0; j < this.height; j++) {
+          res += c;
         }
         console.log(res);
       }
