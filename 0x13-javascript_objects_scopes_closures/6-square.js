6278bd9ec0307b04cd824de8587ad29dfc508b57
UnfazedAy <ayomidesoniyi@gmail.com>
2022-10-07 01:16:19 -0700

    Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,17 @@
 #!/usr/bin/node
-module.exports = class Square extends require('./5-square.js') {
+class Square extends require('./5-square') {
   charPrint (c) {
     if (c === undefined) {
       this.print();
     } else {
-      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+      for (let i = 0; i < this.height; i++) {
+        let res = '';
+        for (let j = 0; j < this.width; j++) {
+          res += 'C';
+        }
+        console.log(res);
+      }
     }
   }
-};
+}
+module.exports = Square;
