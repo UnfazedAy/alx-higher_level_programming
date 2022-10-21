21735f394bd0ed3352e9e94946bf0e973fd16519
UnfazedAy <ayomidesoniyi@gmail.com>
2022-10-21 14:21:51 -0700

    Task 2 done

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,7 +9,7 @@ from sys import argv
 from urllib.parse import urlencode
 if __name__ == '__main__':
     url = argv[1]
-    email = {'email' : argv[2]}
+    email = {'email': argv[2]}
 
     data = urlencode(email).encode('utf-8')
     req = Request(url, data)
