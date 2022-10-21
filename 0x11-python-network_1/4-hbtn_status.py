102685b769602de00b8cffe53dda20de6a15b5b6
UnfazedAy <ayomidesoniyi@gmail.com>
2022-10-21 16:00:50 -0700

    requests are way more simpler than urllib

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,6 +7,6 @@ Using the request package to fetch a url like task 1
 import requests
 if __name__ == '__main__':
     response = requests.get('https://alx-intranet.hbtn.io/status')
-    print('Body response')
+    print('Body response:')
     print('\t- type: {}'.format(type(response.text)))
     print('\t- content: {}'.format(response.text))
