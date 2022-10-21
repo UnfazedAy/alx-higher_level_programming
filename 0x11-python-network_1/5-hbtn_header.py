6e43cf66f71cd812e4a4723a5c38cc4302de3946
UnfazedAy <ayomidesoniyi@gmail.com>
2022-10-21 16:13:23 -0700

    I'm so much loving using requests

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
+"""
+Using the request package to send a request to the URL
+and displays the value of the variable
+X-Request-Id in the response header
+also sys must be used
+"""
 
+
+from sys import argv
+import requests
+if __name__ == '__main__':
+    url = argv[1]
+    my_req = requests.get(url)
+    print(my_req.header.get('X-Request-Id'))
