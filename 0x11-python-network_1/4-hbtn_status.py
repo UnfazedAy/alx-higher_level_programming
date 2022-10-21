8b6d336536c69a4a183ebe6d62756d7b395853b8
UnfazedAy <ayomidesoniyi@gmail.com>
2022-10-21 15:54:43 -0700

    requests are way more simpler than urllib

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Using the request package to fetch a url like task 1
+"""
+
+
+import requests
+if __name__ == '__main__':
+    response = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response')
+    print('\t- type: {}'.format(type(response.text)))
+    print('\t- content: {}.format(response.text))
