53545e96c0035d9f6e31f2aad86f9dbf1a95ac41
UnfazedAy <ayomidesoniyi@gmail.com>
2022-10-21 17:08:14 -0700

    I'm feeling sleepy

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,18 +1,18 @@
-#!/user/bin/python3
+#!/usr/bin/python3
+"""A script that
+- takes in a URL
+- sends a request to the URL
+- displays the body of the response.
 """
-Python script that takes in a URL
-sends a request to the URL and displays the body of the response.
-If the HTTP status code is greater than or equal to 400
-print: Error code: followed by the value of the HTTP status code
-"""
-
-
+import sys
 import requests
-from sys import argv
-if __name__ = '__main__':
-    url = argv[1]
-    req = requests.get(url)
-    if req.status_code >= 400:
-        print(f'Error code: {req.status_code)')
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
     else:
-        print(req.text)
+        print(r.text)
