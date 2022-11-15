8e1caed89c67fa3518fc6405890953a294e7e2d6
UnfazedAy <ayomidesoniyi@gmail.com>
2022-11-15 15:28:22 +0000

    Write files

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ A scriptt that akes in arguments as a file path and content
 and writes in the content to the file
 **/
 
-const { fs } = require('fs');
+const fs = require('fs');
 const { argv } = require('process');
 const filePath = argv[2];
 const content = argv[3];
